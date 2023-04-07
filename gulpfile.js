d144ef5ccb462221ffbf7ce589b8e9d4b38db34c
devonriley <devontriley@gmail.com>
2023-04-07 12:41:30 -0400

    Update gulpfile and root borderRadius

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,27 @@
 const { src, dest, watch, series, parallel } = require( 'gulp' )
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify')
+const browsersync = require("browser-sync").create();
+
+// BrowserSync
+function browserSync(done) {
+    browsersync.init({
+        proxy: 'gateway-arts.vinfen-gateway-multisite.local',
+        open: false,
+        notify: false,
+        ghostMode: false,
+        ui: {
+            port: 8001
+        }
+    });
+    done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+    browsersync.reload();
+    done();
+}
 
 function mainSCSS() {
     return src([
@@ -8,9 +29,11 @@ function mainSCSS() {
     ])
         .pipe(sass().on('error', sass.logError))
         .pipe(dest('.'))
+        .pipe(browsersync.stream());
 }
 
-exports.build = parallel( mainSCSS )
-exports.default = () => {
-    watch( [ './scss/*.scss' ], mainSCSS )
+function watchFiles () {
+    watch( [ './scss/**/*.scss' ], series(mainSCSS, browserSyncReload) )
 }
+exports.build = parallel( mainSCSS )
+exports.default = parallel( browserSync, watchFiles )
