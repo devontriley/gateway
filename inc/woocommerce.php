0de4e6eddd996f461640ae7ec4d53e6cb8ed1f54
devonriley <devontriley@gmail.com>
2023-04-16 08:22:48 -0400

    Update rte fifty fifty bg color

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -1,7 +1,9 @@
 <?php
 
+/*
+ * Send gtag GA4 "add_to_cart" event when product is added to cart from PDP
+ */
 function create_gtag_product( $cartTotal, $products ) {
-
     return array(
         'currency' => 'USD',
         'value' => $cartTotal,
@@ -50,3 +52,34 @@ function custom_add_to_cart() {
 
 <?php }
 add_action('woocommerce_add_to_cart', 'custom_add_to_cart');
+
+/*
+ * Send gtag GA4 "purchase" event on successful purchase
+ * Documentation: https://developers.google.com/analytics/devguides/collection/ga4/ecommerce?client_type=gtag#make_a_purchase_or_issue_a_refund
+ */
+function create_gtag_order( $order ) {
+    return array(
+        'transaction_id' => "T_12345",
+        'value' => 25.42,
+        'tax' => 4.90,
+        'shipping' => 5.99,
+        'currency' => "USD",
+        'coupon' => "SUMMER_SALE",
+        'items' => array()
+    );
+}
+
+function gateway_payment_complete( $order_id ){
+    $order = wc_get_order( $order_id );
+
+    $gtag_order = create_gtag_order( $order );
+    ?>
+
+    <script type="text/javascript">
+        window.addEventListener( 'load', function() {
+            gtag("event", "purchase", <?php echo json_encode( $gtag_order ); ?> );
+            console.log('gtag purchase fired')
+        })
+    </script>
+<?php }
+add_action( 'woocommerce_payment_complete', 'so_payment_complete' );
