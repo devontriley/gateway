54ab3f75cb89a336550c6ea289b3f1654f5beddd
devonriley <devontriley@gmail.com>
2023-05-05 07:33:18 -0400

    Add box shadow to grid cards in Heretic theme (remove styles from Vinfen), add full bleed bg to Heretic, delete grid layout from Gateway

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,13 @@
 const { src, dest, watch, series, parallel } = require( 'gulp' )
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify')
+const rename = require('gulp-rename')
 const browsersync = require("browser-sync").create();
 
 // BrowserSync
 function browserSync(done) {
     browsersync.init({
-        proxy: 'gateway-arts.vinfen-gateway-multisite.local',
+        proxy: 'vinfen-gateway-multisite.local',
         open: false,
         notify: false,
         ghostMode: false,
@@ -23,6 +24,19 @@ function browserSyncReload(done) {
     done();
 }
 
+function js() {
+    return src([
+        '**/*.js',
+        '!**/*.min.js',
+        '!gulpfile.js',
+        '!node_modules/**'
+    ])
+        .pipe(uglify())
+        .pipe(rename({ extname: '.min.js' }))
+        .pipe(dest('.'))
+        .pipe(browsersync.stream());
+}
+
 function mainSCSS() {
     return src([
         './scss/style.scss'
@@ -32,8 +46,19 @@ function mainSCSS() {
         .pipe(browsersync.stream());
 }
 
-function watchFiles () {
-    watch( [ './scss/**/*.scss' ], series(mainSCSS, browserSyncReload) )
+function layoutSCSS() {
+    return src([
+        './layouts/**/*.scss'
+    ])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('layouts'))
+        .pipe(browsersync.stream());
 }
-exports.build = parallel( mainSCSS )
+
+function watchFiles () {
+    watch( [ './layouts/**/*.scss' ], series( layoutSCSS, browserSyncReload ) )
+    watch( [ './scss/**/*.scss' ], series(mainSCSS, browserSyncReload) )
+    watch( [ '**/*.js', '!**/*.min.js', '!gulpfile.js', '!mode_modules/**' ], series( js, browserSyncReload ) )
+}
+exports.build = parallel( layoutSCSS, mainSCSS, js )
 exports.default = parallel( browserSync, watchFiles )
