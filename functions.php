0ad37ceff2468691058ff3ac76da5079f6f8c758
devonriley <devontriley@gmail.com>
2023-06-16 14:56:09 -0400

    Add cache busting to js/css and remove unused admin styles

--- functions.php
+++ functions.php
@@ -22,29 +22,17 @@ if ( wp_get_environment_type() !== 'production' ) {
     add_action( 'wp_head', 'load_bugherd' );
 }
 
-// Register admin styles and scripts
-function gateway_enqueue_admin_script( $hook ) {
-    global $pagenow;
-
-    if( $pagenow === 'post.php' ) {
-        // Gateway theme styles
-        wp_enqueue_style( 'gateway-style', get_stylesheet_uri(), array( 'heretic-style', 'bootstrap' ), time() );
+if (!function_exists('gateway_enqueue_styles')) :
+    function gateway_enqueue_styles() {
+        wp_enqueue_style( 'gateway-style', get_stylesheet_uri(), array( 'heretic-style' ), filemtime(get_stylesheet_directory() . '/style.css') );
     }
-}
-add_action( 'admin_enqueue_scripts', 'gateway_enqueue_admin_script' );
-
-function gateway_enqueue_styles() {
-    wp_enqueue_style( 'gateway-style', get_stylesheet_uri(), array( 'heretic-style' ), wp_get_theme()->get( 'Version' ) );
-}
+endif;
 add_action( 'wp_enqueue_scripts', 'gateway_enqueue_styles' );
 
 // Register scripts
 if (!function_exists('gateway_scripts')) :
     function gateway_scripts() {
-        $postType = get_post_type();
-
-        // Gateway theme scripts
-        wp_enqueue_script( 'gateway-script', get_site_url().'/wp-content/themes/gateway/main.js', array('jquery'), '1.0', true );
+        wp_enqueue_script( 'gateway-script', get_site_url().'/wp-content/themes/gateway/main.js', array('jquery'), filemtime(get_stylesheet_directory() . '/main.js'), true );
     }
 endif;
 add_action( 'wp_enqueue_scripts', 'gateway_scripts' );
